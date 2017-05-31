b30eda3b8fa3a33969b081305a91466e50da003d
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2017-05-31 11:14:29 +0900

    setup with Bootstrap 4

--- brunch-config.js
+++ brunch-config.js
@@ -54,6 +54,12 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: ["node_modules/bootstrap/scss"]
+      },
+      precision: 8
     }
   },
 
@@ -64,6 +70,12 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: {
+      $: 'jquery',
+      jQuery: 'jquery',
+      Tether: 'tether',
+      bootstrap: 'bootstrap'
+    }
   }
 };
