4105228ea706b423ab55b53bf40f4f5a3cdc9d67
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2017-07-01 22:44:10 +0900

    Change text of entry email

--- lib/mokou/email.ex
+++ lib/mokou/email.ex
@@ -1,12 +1,12 @@
 defmodule Mokou.Email do
   use Bamboo.Phoenix, view: Mokou.EmailView
 
-  def entry_complete_email(address, name, id) do
+  def entry_complete_email(address, name, id, count) do
     new_email()
     |> to(address)
     |> from({"針の音楽団整理券システム", "mokou@s01.info"})
     |> subject("[針の音楽団]予約が完了しました")
     |> put_text_layout({Mokou.LayoutView, "email.text"})
-    |> render("entry_complete.text", name: name, id: id)
+    |> render("entry_complete.text", name: name, id: id, count: count)
   end
 end
--- web/controllers/entry_controller.ex
+++ web/controllers/entry_controller.ex
@@ -13,8 +13,8 @@ defmodule Mokou.EntryController do
 
     case Repo.insert(changeset) do
       {:ok, entry} ->
-        %Entry{email: address, name: name, id: id} = entry
-        Email.entry_complete_email(address, name, id) |> Mailer.deliver_later()
+        %Entry{email: address, name: name, id: id, count: count} = entry
+        Email.entry_complete_email(address, name, id, count) |> Mailer.deliver_later()
 
         conn
         |> put_flash(:info, "予約を行いました。登録したEメールアドレスに詳細を送信しました。")
