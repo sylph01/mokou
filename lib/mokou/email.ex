2492eaeed98c3d6d42e2bfb031222309d83cc0a7
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2017-07-06 14:52:02 +0900

    Change address

--- lib/mokou/email.ex
+++ lib/mokou/email.ex
@@ -4,7 +4,7 @@ defmodule Mokou.Email do
   def entry_complete_email(address, name, id, count) do
     new_email()
     |> to(address)
-    |> from({"針の音楽団整理券システム", "mokou@s01.info"})
+    |> from({"針の音楽団整理券システム", "noreply@ticket.harimusic.net"})
     |> subject("[針の音楽団]予約が完了しました")
     |> put_text_layout({Mokou.LayoutView, "email.text"})
     |> render("entry_complete.text", name: name, id: id, count: count)
