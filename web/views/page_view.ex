be593587be06d5d17e9099ed3b84f5758e54e62b
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2017-07-15 15:49:40 +0900

    Change texts and stuff

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -4,9 +4,9 @@ defmodule Mokou.PageView do
 
   def entry_form_button(conn) do
     if EntryHelpers.entry_possible?() do
-      link("予約を行う", to: page_path(conn, :privacy), class: "btn btn-primary btn-lg", role: "button")
+      link("整理券を発行する", to: page_path(conn, :privacy), class: "btn btn-primary btn-lg", role: "button")
     else
-      link("現在予約を行うことができません", to: "#", class: "btn btn-disabled btn-lg", role: "button")
+      link("現在整理券の発行を行うことができません", to: "#", class: "btn btn-disabled btn-lg", role: "button")
     end
   end
 end
