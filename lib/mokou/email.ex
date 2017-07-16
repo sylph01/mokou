b44c31ec447ffb693057a5c5099865c8fd474cc8
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2017-07-16 18:06:37 +0900

    Add Reply-To header

--- lib/mokou/email.ex
+++ lib/mokou/email.ex
@@ -3,11 +3,12 @@ defmodule Mokou.Email do
 
   def entry_complete_email(address, name, id, count) do
     sender_in_iso2022jp  = :creole.convert("針の音楽団整理券システム", :utf8, :iso_2022_jp_1)
-    
+
     new_email()
     |> to(address)
     |> from({sender_in_iso2022jp, "noreply@ticket.harimusic.net"})
     |> subject("[針の音楽団]演奏会整理券のお知らせ")
+    |> put_header("Reply-To", "harimusic896@gmail.com")
     |> put_text_layout({Mokou.LayoutView, "email.text"})
     |> render("entry_complete.text", name: name, id: id, count: count)
   end
