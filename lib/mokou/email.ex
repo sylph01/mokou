f12446cbbf0575c10b2ca6677774fbedac42a4c6
Ryo Kajiwara <kfe-fecn6.prussian@s01.info>
2017-07-18 23:06:43 +0900

    Add Precedence and Message-ID header

--- lib/mokou/email.ex
+++ lib/mokou/email.ex
@@ -7,6 +7,8 @@ defmodule Mokou.Email do
     |> from({"針の音楽団整理券システム", "noreply@ticket.harimusic.net"})
     |> subject("[針の音楽団]演奏会整理券のお知らせ")
     |> put_header("Reply-To", "harimusic896@gmail.com")
+    |> put_header("Precedence", "bulk")
+    |> put_header("Message-ID", "harimusicticket#{id}")
     |> put_text_layout({Mokou.LayoutView, "email.text"})
     |> render("entry_complete.text", name: name, id: id, count: count)
   end
